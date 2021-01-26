1bccec1379110070d1eddfd76478ccb4e4c3ef10
suz15anne <78038596+suz15anne@users.noreply.github.com>
2021-01-26 11:49:53 -0500

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class Hello{
+	public static void main(String[] args) {
+		
+	 
+		System.out.println("Suzanne Ndjoumbe");
+		System.out.println("Introduction");
+		System.out.println("Lab 01");
+		System.out.println("01/25/2021");
+		System.out.println("CMCS 255-003");
+		System.out.println();
+	}
+}	
+
